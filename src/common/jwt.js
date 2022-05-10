a7b0501cfbcf30b8d388aed1b257be16eafbdea2
ALAN DAVID <46442525@ort>
2022-05-10 08:56:10 -0300

    try catch requiring

--- src/common/jwt.js
+++ src/common/jwt.js
@@ -7,6 +7,8 @@ const opt = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   issuer: `${process.env.AUTH_ISSUER_URL}`,
   algorithms: ["HS256"],
+  audience: `${process.env.AUTH_AUDIENCE_URL}`,
+  payload: `${process.env.AUTH_PAYLOAD}`
 };
 
 export const jwtStrategy = new Strategy(opt, (jwt_payload, done) => {
